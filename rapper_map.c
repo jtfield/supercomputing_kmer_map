546f806b57c2c892844a2c82ad9fe59a59f56b0f
iCER Admin <jtoscanifield@ucmerced.edu>
2019-07-08 17:06:10 +0800

    more efficient file parsing. starting to generate kmers

--- rapper_map.c
+++ rapper_map.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#define MAX 300    /* define constants, don't use magic number in code */
+#include <string.h>
+// #define MAX 300    /* define constants, don't use magic number in code */
 
-
+// int main () {
 int main ( int argc, char *argv[] ) {
-   FILE *fp;
-   char arr[500];
-   // int linecount = 0;
-   int readcount;
-   int linecount;
-
+/*
+  FILE *fp;
+  char *arr[500];
+  // int linecount = 0;
+  int readcount;
+  int linecount;
+/*
    if (argc == 2){
      fp = fopen(argv[1], "r"); // "r" for read
      readcount = 0;
@@ -22,6 +24,7 @@ int main ( int argc, char *argv[] ) {
          // puts(str);
          if (readcount == 2) {
            printf("%s", str);
+           arr[readcount] = str;
          }
          else if (readcount == 4) {
            readcount = 0;
@@ -29,47 +32,68 @@ int main ( int argc, char *argv[] ) {
 
         }
       }
-     printf("%d", readcount);
-     printf("%d", linecount);
-     fclose(fp);
-     return 0;
-   }
-   //   do {
-   //      c = fgets(fp);
-   //      if( feof(fp) ) {
-   //         break ;
-   //
-   //      printf("%c", c);
-   //
-   //      printf("\n");
-   //   } while(1);
-   //
-   //   fclose(fp);
-   //   return(0);
-   // }
-   // fp = fopen("./small_test_dataset/newClade_1_01.R1_.fastq","r");
-   // m = 1;
-   // j = m<<2;
-   // if(fp == NULL) {
-   //    perror("Error in opening file");
-   //    return(-1);
-   // }
-   // do {
-   //    c = fgetc(fp);
-   //    if( feof(fp) ) {
-   //       break ;
-   //    }
-   //    if(c == 'A') {
-   //      printf("%d", j);
-   //    }
-   //    printf("%c", c);
-   //    // printf("%ld", sizeof(c));
-   //    // d = c<<1;
-   //    // printf("%d", d);
-   //    // printf("%ld", sizeof(d));
-   //    printf("\n");
-   // } while(1);
-   //
-   // fclose(fp);
-   // return(0);
+*/
+// printf("%d", readcount);
+// printf("%d", linecount);
+// printf("%s", arr);
+// fclose(fp);
+  FILE *fptr;
+  fptr = fopen(argv[1], "r"); // "r" for read
+  // fptr = fopen("small_test_dataset/newClade_1_01.R1_.fastq","r");
+  char data[40][2][400];
+  int i,j,k,loop;
+
+
+  for(i = 0; i < 40; i++)
+  {
+    fscanf(fptr,"%s",data[i][0]);
+    fscanf(fptr,"%s",data[i][0]);
+    fscanf(fptr,"%s",data[i][0]);
+    fscanf(fptr,"%s",data[i][1]);
+    fscanf(fptr,"%s",data[i][1]);
+  }
+  // printf("sssssss j = %d  j = %d\n",i,j);
+  // for(loop = 0; loop < 10; loop++)
+  // {
+  //   printf("%s\n", data[loop][0]);
+  //   printf("%s\n", data[loop][1]);
+  // }
+
+
+  char *string;
+  char nuc;
+  printf("SSSSSSSSSS\n");
+
+
+  char read_kmer[40][7][16];
+  for(i = 0; i < 40; i++)
+  {
+    string = data[i][0];
+    // printf("%s\n", string);
+    // printf("%d\n", i);
+
+    for(j = 0; j < 14; j++)
+    {
+      nuc = string[j];
+      // printf("%c", nuc);
+      read_kmer[i][0][j] = nuc;
+      // printf("%c", string[j]);
+      // fscanf(string[j], "%c", read_kmer[i][0][j]);
+      // read_kmer[i][0][j]
+    }
+    // printf("\n");
+
+  }
+  printf("sssssss\n");
+  for(loop = 0; loop < 40; loop++)
+  {
+    printf("%s\n", read_kmer[loop][0]);
+    printf("%d\n", loop);
+  }
+  // printf("%s", data);
+  fclose(fptr);
+
+
+
+  return 0;
 }
