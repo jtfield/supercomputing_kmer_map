6836ff65fd1be3afdb2baeaf7f554598d51fa86c
iCER Admin <jtoscanifield@ucmerced.edu>
2019-07-12 16:34:32 +0800

    trying to construct the main kmer search

--- rapper_map.c
+++ rapper_map.c
@@ -8,44 +8,40 @@
 #define PART_NUMBER 150
 #define LOCI_COUNT 10
 #define LOCI_LEN 5000
+#define READ_KMER_NUM 30
 
 
 // Function to cut reads into PART_SIZE bp kmers
-void divide_read(char *str, char *read_kmer)
+void divide_read(char *seq, char *read_kmer)
 {
-  int str_size = strlen(str);
+  int seq_size = strlen(seq);
   int i;
 
-  int copy_size = str_size - str_size % PART_SIZE;
+  int copy_size = seq_size - seq_size % PART_SIZE;
   // printf("%d\n", copy_size);
 
   for(i = 0;i < copy_size; i++)
-    read_kmer[i] = str[i];
+    read_kmer[i] = seq[i];
 
 }
 
 // Function to cut MSA sequences into kmers, overlapping by k - 1 nucleotides along the sequence length
-void divide_sequence(char *str, char *genome_kmer)
+void divide_sequence(char *seq, char *genome_kmer)
 {
-  int str_size = strlen(str);
+  int seq_size = strlen(seq);
   int i, j, kmer_size;
 
-  // int copy_size = str_size - str_size % PART_SIZE;
-  // printf("%d\n", copy_size);
-
-  for(i = 0; i < str_size; i++)
+  for(i = 0; i < seq_size; i++)
   {
     // j = i;
     kmer_size = PART_SIZE + i;
     for(j = i; j < kmer_size; j++)
     {
-      // genome_kmer[j] = str[j];
-      printf("%c", str[j]);
+      genome_kmer[j] = seq[j];
+      printf("%c", seq[j]);
     }
     printf("\n");
   }
-
-
 }
 
 // int main () {
@@ -61,26 +57,26 @@ int main ( int argc, char *argv[] ) {
 
   char msa[LOCI_COUNT][1][LOCI_LEN];
   char data[PART_NUMBER][2][400];
-  char read_kmer[PART_NUMBER][30][PART_SIZE];
+  // char read_kmer[PART_NUMBER][30][PART_SIZE];
   char msa_kmer[LOCI_COUNT][LOCI_LEN][PART_SIZE];
+  // char best_msa_kmer_match_locs[]
 
-  int i,j,k,loop,nuc_count;
+  int i,j,k,x,n,z,loop,nuc_count;
   int num_part = 0;
   int loci_count = 0;
 
-
+// READ READS FILE IN TO ARRAY
   while(fscanf(fptr,"%s",data[i][0]) != EOF)
   {
     fscanf(fptr,"%s",data[i][0]);
     fscanf(fptr,"%s",data[i][0]);
-    //fscanf(fptr,"%s",data[i][0]);
     fscanf(fptr,"%s",data[i][1]);
     fscanf(fptr,"%s",data[i][1]);
     i++;
   }
-  num_part = i;
-
-  printf("%d\n", i);
+  // num_part = i;
+  //
+  // printf("%d\n", i);
   // for(i = 0; i < num_part; i++)
   // {
   //   printf("i = %d\n\n\n",i);
@@ -88,71 +84,81 @@ int main ( int argc, char *argv[] ) {
   //   printf("%s\n",data[i][1]);
   // }
 
-  j = 0;
-  while(fscanf(msaptr,"%s",msa[j][0]) != EOF)
+
+// READ IN MSA FILE TO ARRAY
+  k = 0;
+  while(fscanf(msaptr,"%s",msa[k][0]) != EOF)
   {
-    fscanf(msaptr,"%s",msa[j][0]);
-    // fscanf(msaptr,"%s",msa[j][1]);
-    j++;
-  }
-  // num_part = i;
-
-
-  printf("%d\n", j);
-
-
-  for(i = 0; i < LOCI_COUNT; i++)
-  {
-    printf("%s\n", msa[i][0]);
-    printf("SSSSSSSSSSSSSSSSSSSSS\n");
+    fscanf(msaptr,"%s",msa[k][0]);
+    k++;
   }
 
-  // printf("sssssss j = %d  j = %d\n",i,j);
-  // for(loop = 0; loop < 10; loop++)
-  // {
-  //   printf("%s\n", data[loop][0]);
-  //   printf("%s\n", data[loop][1]);
-  // }
+
+  for(n = 0; n < 1; n++)
+  {
+    char *str = msa[n][0];
+    // printf("%s\n\n", str);
+    char *read;
+    char *kmer;
+    int seq_size = strlen(str);
+    char current_msa_kmer_match_locs[1][seq_size][PART_SIZE];
+    // char read_kmer[PART_NUMBER][30][PART_SIZE];
+
+    int kmer_size;
+
+    for(x = 0; x < PART_NUMBER; x++)
+    {
+      char read_kmer[1][30][PART_SIZE];
+      read = data[x][0];
+      printf("%s\n\n", read);
+      int read_size = strlen(read);
+      int copy_size = read_size - read_size % PART_SIZE;
+      printf("%d\n",copy_size);
+      for(i = 0;i < copy_size; i++)
+      {
+        read_kmer[i] = read[i];
+      }
+      // for(i = 0; i < read_size; i++)
+      // {
+      //   j = i;
+      //   kmer_size = PART_SIZE + i;
+      //   for(j = i; j < kmer_size; j++)
+      //   {
+      //     // current_msa_kmer_match_locs[0][i][j] = str[j];
+      //     printf("%c", str[j]);
+      //   }
+      //   printf("\n");
+      // }
+
+      // for(z = 0; z < READ_KMER_NUM; z++)
+      // {
+      //   kmer = read_kmer[z][0];
+      //   printf("%s\n", kmer);
+      // }
+
+      // for(k = 0;k < 14; k++){
+      //     printf("%c",read_kmer[0][0][k]);
+      // }
+      // printf("DDDDDDDDDDDDDDDDDDDDDDDDDDD\n");
+      // printf("%s\n", str);
+      // for(i = 0; i < str_size; i++)
+      // {
+      //   j = i;
+      //   kmer_size = PART_SIZE + i;
+      //   for(j = i; j < kmer_size; j++)
+      //   {
+      //     // current_msa_kmer_match_locs[0][i][j] = str[j];
+      //     printf("%c", str[j]);
+      //   }
+      //   printf("\n");
+      // }
+    }
+  }
+
+
 
   printf("Finish Read\n");
-  for(i = 0; i < num_part; i++)
-  {
-    char *str = data[i][0];
-    // printf("dddd\n the first kmer part   ");
-    divide_read(str, &read_kmer[i][0][0]);
 
-    // for(k = 0;k < 14; k++)
-    //   printf("%c",read_kmer[i][0][k]);
-    // printf("\n the second kmer part  ");
-    // for(k = 0;k < 14; k++)
-    //   printf("%c",read_kmer[i][1][k]);
-    // printf("\n the third kmer part  ");
-    // for(k = 0;k < 14; k++)
-    //   printf("%c",read_kmer[i][2][k]);
-
-
-    //printf("%s\n",read_kmer[i][1]);
-    // printf("\n\n\n\n");
-    //getchar();
-  }
-
-  printf("Beginning MSA kmer cutting\n");
-  for(i = 0; i < LOCI_COUNT; i++)
-  {
-    char *str = msa[i][0];
-    divide_sequence(str, &msa_kmer[i][0][0]);
-
-  //   for(k = 0; k < LOCI_COUNT; k++)
-  //   {
-  //     printf("%s", msa_kmer[i][0][k]);
-  //   }
-  }
-  printf("Finished MSA kmer cutting\n");
-
-  // for(k = 0; k < LOCI_COUNT; k++)
-  // {
-  //   printf("%s", msa_kmer[i][0][k]);
-  // }
 
   return 0;
 }
