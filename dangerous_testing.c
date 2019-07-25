d835fb764029e33c6cf5c4855a2918e2b7ba083d
iCER Admin <jtoscanifield@ucmerced.edu>
2019-07-25 16:34:31 +0800

    adjusting for mpi processing

--- dangerous_testing.c
+++ dangerous_testing.c
@@ -4,50 +4,17 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <mpi.h>
 #define KMER_SIZE 14
 #define READ_NUMBER 5001
 #define LOCI_COUNT 10
-#define LOCI_LEN 5000000
+#define LOCI_LEN 100
 #define READ_KMER_NUM 30
 //#define N_ROW 5001
 //#define N_COL 2
 //#define N_CHAR 300
-
-// Function to cut reads into PART_SIZE bp kmers
-void divide_read(char *seq, char *read_kmer)
-{
-  int seq_size = strlen(seq);
-  int i;
-
-  int copy_size = seq_size - seq_size % KMER_SIZE;
- // printf("%d\n", copy_size);
-
-  for(i = 0;i < copy_size; i++)
-    read_kmer[i] = seq[i];
-
-}
-
-// Function to cut MSA sequences into kmers, overlapping by k - 1 nucleotides along the sequence length
-void divide_sequence(char *seq, char *genome_kmer)
-{
-  int seq_size = strlen(seq);
-  int i, j, kmer_size;
-
-  for(i = 0; i < seq_size; i++)
-  {
-    // j = i;
-    kmer_size = KMER_SIZE + i;
-    for(j = i; j < kmer_size; j++)
-    {
-      genome_kmer[j] = seq[j];
-      printf("%c", seq[j]);
-    }
-    printf("\n");
-  }
-}
-
-// int main () {
-
+#define send_data_tag 2001
+#define return_data_tag 2002
 
 int main ( int argc, char *argv[] ) {
 
@@ -58,7 +25,7 @@ int main ( int argc, char *argv[] ) {
   char read_count_buf[400];
   //char msa[LOCI_COUNT][1][LOCI_LEN];
   char read_buf[400];
-  //char data[READ_NUMBER][2][600];
+  char data2[READ_NUMBER][300];
   char msa_buf[LOCI_LEN];
   int i = 0;
   int j,k,x,n,z,loop;
@@ -74,6 +41,20 @@ int main ( int argc, char *argv[] ) {
   int msa_arrlen = MSA_STEPSIZE;
   const int MATCH_STEPSIZE = 1000;
   int match_arrlen = MATCH_STEPSIZE;
+  MPI_Status status;
+  int my_id, root_process, ierr, num_rows, num_procs,
+         an_id, num_rows_to_receive, avg_rows_per_process, 
+         sender, num_rows_received, start_row, end_row, num_rows_to_send;
+  ierr = MPI_Init(&argc, &argv);
+  root_process = 0;
+  ierr = MPI_Comm_rank(MPI_COMM_WORLD, &my_id);
+  ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+  if(my_id == root_process)
+  {
+
+
+
 
 
 // CONSTRUCT A DYNAMIC MEMORY ARRAY TO HOLD THE READS WITHOUT REQUIRING KNOWING HOW MANY READS ARE IN THE FILE
@@ -167,229 +148,60 @@ int main ( int argc, char *argv[] ) {
         k = 0;
 
     }
-  }
-
-//############################################################################################
+   }
 
 
-//START DYNAMIC MATCH ARRAY
-//############################################################################################
- 
-int **match = (int **)malloc(MATCH_STEPSIZE * sizeof(int *));
+  printf("%s\n", data[0]);
 
-//############################################################################################
- 
-  
-  
-  
-  
-  
-  
-  /* KMER HASHING AND MATCHING CODE BLOCK
-  * START BY LOOPING THROUGH EACH SEQUENCE IN THE MSA */
+  avg_rows_per_process = total_read_count / num_procs;
+  printf("average number of reads per processor = %d\n", avg_rows_per_process);
 
-  for(n = 0; n < total_seq_count; n++)
+  for(an_id = 1; an_id < num_procs; an_id++)
   {
-    char *str = msa[n];
-    //printf("%s\n", str);
-    //char *read;
-    char *kmer;
-    char *nuc;
-    int seq_size = strlen(str);
-    //int kmer_size;
-    int safe_hasher = 0;
+    start_row = an_id*avg_rows_per_process + 1;
+    end_row   = (an_id + 1)*avg_rows_per_process;
 
-   /* LOOP THROUGH READS AND BEGIN CUTTING INTO KMERS */
-    printf("BEGIN LOOPING THROUGH READS\n");
-    for(x = 0; x < total_read_count; x++)
+    if((total_read_count - end_row) < avg_rows_per_process)
+      end_row = total_read_count - 1;
+
+    num_rows_to_send = end_row - start_row + 1;
+
+    ierr = MPI_Send( &num_rows_to_send, 1 , MPI_CHAR,
+           an_id, send_data_tag, MPI_COMM_WORLD);
+
+    ierr = MPI_Send( &data[start_row][0], num_rows_to_send, MPI_CHAR,
+           an_id, send_data_tag, MPI_COMM_WORLD);
+   }
+  }
+  else
+  {
+    ierr = MPI_Recv( &num_rows_to_receive, 1, MPI_INT, 
+               root_process, send_data_tag, MPI_COMM_WORLD, &status);
+          
+    ierr = MPI_Recv( &data2, num_rows_to_receive, MPI_INT, 
+               root_process, send_data_tag, MPI_COMM_WORLD, &status);
+
+    num_rows_received = num_rows_to_receive;
+    for(i = 0;i < num_rows_received; i++);
     {
-
-      //char kmers_for_read[1][READ_KMER_NUM][KMER_SIZE];
-      char *read = data[x];
-      //printf("%s\n", read);
-      //printf("READ NUMBER = %d LOCI NUMBER = %d\n", x, n);
-      //int read_hash_array[read_count][READ_KMER_NUM];
-      //int read_hash_array[1][READ_KMER_NUM];
-      //int ** read_hash_array = (int ** )malloc(2 * sizeof(int *));
-      //for(i = 0; i < N_ROW; i++)
-      //{
-	//read_hash_array[i] = (int *)malloc(READ_KMER_NUM * sizeof(int));
-      //}
-      int read_size = strlen(read);
-      //printf("read size = %d\n", read_size);
-      int read_km_number = read_size / KMER_SIZE;
-      int read_hash_array[1][read_km_number + 2];
-      //char kmers_for_read[1][read_km_number + 2][KMER_SIZE + 2];
-      int trimmed_read_size = read_size - read_size % KMER_SIZE;
-      //printf("11111read_km_number %d\n", read_km_number);
-      //divide_read(read, &kmers_for_read[x][0][0]);
-      //printf("22222read_km_number %d\n", read_km_number);
-      //printf("kmer nucleotide size %d\n", KMER_SIZE);
-     int window = 0;
-     for(j = 0; j < read_km_number; j++)
-     {
-       //printf("%d\n", window);
-       int read_hash_number = 0;
-       //int window = 0;
-       for (i = window; i < KMER_SIZE * (j + 1); i++)
-       {
-         window++;
-         int read_hash = 0;
-         int ps = read[i];
-         //printf("%c",ps); 
-        
-      /* HASHES GENERATED FOR THE KMERS FROM THE READ */
-      //printf("***SPLITTING AND HASHING READ***\n");
-	  switch(ps)
-	  {
-	    case 'A':
-	    case 'a':
-	      read_hash = 0;
-	      break;
-	    case 'C':
-            case 'c':
-              read_hash = 1;
-              break;
-	    case 'G':
-            case 'g':
-              read_hash = 2;
-              break;
-	    case 'T':
-            case 't':
-              read_hash = 3;
-              break;
-	    default :
-	      read_hash = 4;
-	  }
-          read_hash_number = read_hash_number * 4 + read_hash;
-	  read_hash = 0;
-        }
-        //printf("\n");
-        read_hash_array[0][j] = read_hash_number;
-        //printf("mas = %d\n", read_hash_number);
-        //printf("reset mas = %d\n", read_hash_number);
-        //printf("\n\n\n");
-        read_hash_number = 0;
-      }
-
-
-      //printf("***SEQUENCE HASHING SLIDING WINDOW***\n");
-      int msa_hash_number = 0;
-      for(i = 0 ; i < seq_size; i++)
+      for(j = 0; j < 300; j++)
       {
-
-      //printf("%c\n", str[i]);
-        int hash = 0;
-        //printf("  hash = %d  ", hash);
-	switch(str[i])
-        {
-          case 'A':
-          case 'a':
-            hash = 0;
-            break;
-          case 'C':
-          case 'c':
-            hash = 1;
-            break;
-          case 'G':
-          case 'g':
-            hash = 2;
-            break;
-          case 'T':
-          case 't':
-            hash = 3;
-            break;
-	  default :
-	    hash = 4;
-        }
-        //hash = B_Char_map_Int[str[i] - '0'];
-       // printf("   new hash= %d  ", hash);
-        //int sub_hash = B_Char_map_Int[str[i - KMER_SIZE] - '0'];
-	int sub_hash = 0;
-	switch(str[i - KMER_SIZE])
-        {
-          case 'A':
-          case 'a':
-            sub_hash = 0;
-            break;
-          case 'C':
-          case 'c':
-            sub_hash = 1;
-            break;
-          case 'G':
-          case 'g':
-            sub_hash = 2;
-            break;
-          case 'T':
-          case 't':
-            sub_hash = 3;
-            break;
-	  default :
-	    sub_hash = 4;
-        }
-        //printf("sub = %d has = %d\n", sub_hash, hash);
-        msa_hash_number = msa_hash_number * 4 + hash - (sub_hash << 28);
-        //printf("MSA_HASH_NUMBER_   %d\n", msa_hash_number);
-	//printf("%d\n",msa_hash_number);
-        for(j = 0; j < read_km_number; j++)
-        {
-	 //printf("%d\n",msa_hash_number);
-         //printf("read_kmer = %d\n", read_hash_array[0][j]);
-         if(read_hash_array[0][j] == msa_hash_number)
-          {
-//############################################################
-            //printf("FOUND HASH MATCH seq position:%d in loci: %d read kmer:%d of read %d\n", i, n, j, x);
-            //printf("%s\n", read);
-	    //match_count++;
-	    if(match_count == match_arrlen)
-	    {
-	      match_arrlen += MATCH_STEPSIZE;
-              int ** match_newlines = realloc(match, match_arrlen * sizeof(int * ));
-              if (!match_newlines)
-                {
-                  fprintf(stderr, "can't realloc\n");
-                  exit(1);
-                }
-              match = match_newlines;
-	    }
-	    match[match_count] = (int *)malloc(4 * sizeof(int));
-	    match[match_count][0] = x;
-	    match[match_count][1] = j;
-	    match[match_count][2] = n;
-	    match[match_count][3] = i;
-	    match_count++;
-	    
-//############################################################
-	  }
-        }
-	hash = 0;
-	sub_hash = 0;
-
-
+	      printf("%s",data2[i]);
       }
-      msa_hash_number = 0;
-
+      printf("\n");
     }
 
-   }
-  //free(data);
-  
-  for(i = 0; i < match_count; i++)
-  {
-	  for(j = 0; j < 4; j++)
-	  {
-		  printf("  %d   ", match[i][j]);
-		  //free(&match[i][j]);
-	  }
-	  printf("\n");
-          //free(match[i]);
+
+
+
+
+    printf("PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP\n");
+    printf("Hello world! I'm process %i out of %i processes\n",
+         my_id, num_procs);
   }
-  //free(match);
- 
-  //printf("%d", n);
   
   printf("Finish Read\n");
-
+  ierr = MPI_Finalize();
 
   return 0;
 }
